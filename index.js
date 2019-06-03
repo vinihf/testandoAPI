4e43094c5e7c62ca64c2dded34763d60cdbe52b4
Vinicius H. Ferreira <vinihf@gmail.com>
2019-06-03 11:06:42 -0300

    v

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser')
 var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
@@ -10,6 +14,13 @@ app.get('/dobro/:n', function (req, res) {
     obj = {'number':req.params.n,"result":result};
     myObj = JSON.stringify(obj);
     res.type("json").send(myObj);
+});
+
+app.get('/triplo/', function (req, res) {
+  let result = parseInt(req.body.n)*3;
+  obj = {'number':req.body.n,"result":result};
+  myObj = JSON.stringify(obj);
+  res.type("json").send(myObj);
 });
 
 var port = process.env.PORT || 8080;
