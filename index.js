6b9c5711960aa79af5717b98dbb0b03af1dbef85
Vinicius H. Ferreira <vinihf@gmail.com>
2019-06-04 15:53:36 -0300

    cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser')
+var cors = require('cors');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
